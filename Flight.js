976745ecac0d54f0c06a9698296cae60c3544366
Johan <102482684+Wol-ZA@users.noreply.github.com>
2025-07-08 10:52:15 +0200

    Update Flight.js

--- Flight.js
+++ Flight.js
@@ -1331,25 +1331,85 @@ for (let i = 0; i < polylineCoordinates.length - 1; i++) {
   draggableGraphicsLayer.add(arrow);
 
   // 2B. Add text label at midpoint
-  const textGraphic = new Graphic({
-    geometry: {
-      type: "point",
-      longitude: midX,
-      latitude: midY
-    },
-    symbol: {
-      type: "text",
-      text: `${distance} nm\n${Math.round(bearing)}° MB`,
-      color: "black",
-      font: {
-        size: 10,
-        weight: "bold"
-      },
-      haloColor: "white",
-      haloSize: 1,
-      yoffset: 10
+ const textGraphic = new Graphic({
+  geometry: {
+    type: "point",
+    longitude: midX,
+    latitude: midY
+  },
+  symbol: {
+    type: "cim",
+    data: {
+      type: "CIMSymbolReference",
+      symbol: {
+        type: "CIMPointSymbol",
+        symbolLayers: [
+          {
+            type: "CIMVectorMarker",
+            size: 50,
+            frame: {
+              xmin: -25,
+              ymin: -15,
+              xmax: 25,
+              ymax: 15
+            },
+            markerGraphics: [
+              // Background rectangle
+              {
+                type: "CIMMarkerGraphic",
+                geometry: {
+                  rings: [[
+                    [-25, -15],
+                    [25, -15],
+                    [25, 15],
+                    [-25, 15],
+                    [-25, -15]
+                  ]]
+                },
+                symbol: {
+                  type: "CIMSolidFill",
+                  color: [255, 255, 255, 255] // white
+                }
+              },
+              // Border
+              {
+                type: "CIMMarkerGraphic",
+                geometry: {
+                  rings: [[
+                    [-25, -15],
+                    [25, -15],
+                    [25, 15],
+                    [-25, 15],
+                    [-25, -15]
+                  ]]
+                },
+                symbol: {
+                  type: "CIMSolidStroke",
+                  width: 1,
+                  color: [0, 0, 0, 255] // black border
+                }
+              },
+              // Text
+              {
+                type: "CIMMarkerGraphic",
+                geometry: { x: 0, y: 0 },
+                symbol: {
+                  type: "CIMTextSymbol",
+                  fontFamilyName: "Arial",
+                  height: 8,
+                  horizontalAlignment: "Center",
+                  verticalAlignment: "Center",
+                  color: [0, 0, 0, 255],
+                  text: `${distance} nm\n${Math.round(bearing)}° MB`
+                }
+              }
+            ]
+          }
+        ]
+      }
     }
-  });
+  }
+});
   draggableGraphicsLayer.add(textGraphic);
 }
 
