45bdffcd60cd56c8113c80ac815db38aaa956827
Johan <102482684+Wol-ZA@users.noreply.github.com>
2025-10-28 09:06:42 +0200

    Update Flight.js

--- Flight.js
+++ Flight.js
@@ -1452,6 +1452,7 @@ const arrow = new Graphic({
     url: "data:image/svg+xml;base64," + btoa(chevronSVG),
     width: "20px",
     height: "20px",
+	style: "triangle",
     angle: angle + 180, // rotate to match SVG’s base orientation
     xoffset: 0,
     yoffset: 0
